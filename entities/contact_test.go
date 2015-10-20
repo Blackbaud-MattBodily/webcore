5a0518179f2baeea6e097bc6f74efd98aa93c960
Erik Tate <that_dude_over_there@hotmail.com>
2015-10-20 17:53:37 -0400

    Added GetContactCount as a standard handler. Worth discussion on what service this function resides in. Right now it's in the ContactService because it's technically a query on the Contact object, but it might make more sense to logically to add it to the AccountService.

---
+++ entities/contact_test.go
@@ -0,0 +1,11 @@
+package entities
+
+import (
+	"testing"
+
+	. "github.com/blackbaudIT/webcore/Godeps/_workspace/src/github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewContact(t *testing.T) {
+	Convey("")
+}
