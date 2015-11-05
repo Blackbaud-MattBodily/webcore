7bb27af9c1a225757ae6daeb33559fd8869a9e13
Matt Smith <ma314smith@users.noreply.github.com>
2015-11-05 15:00:35 -0500

    adding debug logging

--- handlers/assethandler.go
+++ handlers/assethandler.go
@@ -25,7 +25,7 @@ func (h *AssetHandler) GetAssetsByAccountID(w http.ResponseWriter, r *http.Reque
 	vars := mux.Vars(r)
 	accountID, ok := vars["accountId"]
 	if !ok {
-		log.Printf("AssetHandler.GetAssetsByAccountID - missing accountID parameter. vars:%v", vars)
+		log.Println("AssetHandler.GetAssetsByAccountID - missing accountID parameter.")
 		http.Error(w, http.StatusText(400), 400)
 		return
 	}
@@ -38,6 +38,7 @@ func (h *AssetHandler) GetAssetsByAccountID(w http.ResponseWriter, r *http.Reque
 	}
 
 	data, err := json.Marshal(assets)
+	log.Printf("asset data: %v", data)
 	if err != nil {
 		log.Printf("AssetHandler.GetAssetsByAccountID failed to marshal result: %s", err)
 		http.Error(w, http.StatusText(500), 500)
